5476fa84bb1fc244c10b4f41d0879109759f7812
smrsun4 <smrsun4@gmail.com>
2024-06-30 16:39:41 -0400

    entered github questions into index.js and return for readme file

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs')
+const fs = require('fs');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
-    message: 'Press ENTER to continue...',
-    name: 'enterKey',
+    type: 'input'
+    message: 'What is the title of your project?',
+    name: 'title',
+  },{
+    type: 'input'
+    message: 'Please write a short description of your project.',
+    name: 'decription',
+  },{
+    type: 'list'
+    message: 'What type of license, if any, should your project have?',
+    name: 'license',
+    choices: ["MIT", "GNU", "Apache", "Mozilla", "BSD", "none"]
+  },{
+    type: 'input'
+    message: 'What is your Github username?',
+    name: 'userName',
+  },{
+    type: 'input'
+    message: 'What is your Github email address?',
+    name: 'emailAddress',
+  },{
+    type: 'input'
+    message: 'What command should be run to install dependencies',
+    name: 'command',
+  },{
+    type: 'input'
+    message: 'What command should run for testing?',
+    name: 'tests',
+  },{
+    type: 'input'
+    message: 'What does the user need to know to about using the repo?',
+    name: 'usage',
+  },{
+    type: 'input'
+    message: 'What does the user need to know about contributing to the repo?',
+    name: '',
   },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fileName + data;
+}
 
 // TODO: Create a function to initialize app
 function init() {
