8ff92b312c7189856a3dec9c2b8f7f192ae14ef3
smrsun4 <smrsun4@gmail.com>
2024-06-30 14:09:48 -0400

    made the fs

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs')
 
-inquirer
-  .prompt([
-    {
-      message: 'Press ENTER to continue...',
-      name: 'enterKey'
-    }
-  ]);
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    message: 'Press ENTER to continue...',
+    name: 'enterKey',
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions);
+}
+
+// Function call to initialize app
+init();
