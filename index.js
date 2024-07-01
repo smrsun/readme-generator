7fc461cd07e888b2eb696cbeb2af557b9a0f0ec9
smrsun4 <smrsun4@gmail.com>
2024-07-01 18:38:02 -0400

    corrected some simple syntax issues

--- index.js
+++ index.js
@@ -2,53 +2,58 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { default: Choices } = require('inquirer/lib/objects/choices');
+const generateMarkdown = require('./readme-generator/utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
-    type: 'input'
+    type: 'input',
     message: 'What is the title of your project?',
     name: 'title',
   },{
-    type: 'input'
+    type: 'input',
     message: 'Please write a short description of your project.',
     name: 'decription',
   },{
-    type: 'list'
+    type: 'list',
     message: 'What type of license, if any, should your project have?',
     name: 'license',
     choices: ["MIT", "GNU", "Apache", "Mozilla", "BSD", "none"]
   },{
-    type: 'input'
+    type: 'input',
     message: 'What is your Github username?',
     name: 'userName',
   },{
-    type: 'input'
+    type: 'input',
     message: 'What is your Github email address?',
     name: 'emailAddress',
   },{
-    type: 'input'
+    type: 'input',
     message: 'What command should be run to install dependencies',
     name: 'command',
   },{
-    type: 'input'
+    type: 'input',
     message: 'What command should run for testing?',
     name: 'tests',
   },{
-    type: 'input'
+    type: 'input',
     message: 'What does the user need to know to about using the repo?',
     name: 'usage',
   },{
-    type: 'input'
+    type: 'input',
     message: 'What does the user need to know about contributing to the repo?',
-    name: '',
+    name: 'contribution',
   },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  return fileName + data;
-}
+// function writeToFile(fileName, data) {
+  const writeToFile = (data) => { 
+    fs.writeToFile('questions', data, (err) => {
+      err ? console.error(err) : console.log('File created!')
+    })
+  }
+
 
 // TODO: Create a function to initialize app
 function init() {
