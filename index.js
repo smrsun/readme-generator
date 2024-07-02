0405bdc741ec568c57b346be4dfee08c48b73533
smrsun4 <smrsun4@gmail.com>
2024-07-02 18:52:08 -0400

    wrote function to initialize the app and adjusted the writetofile function to hopefully make it work

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { default: Choices } = require('inquirer/lib/objects/choices');
-const generateMarkdown = require('./readme-generator/utils/generateMarkdown.js')
+const generateMarkdown = require('./readme-generator/utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -10,54 +9,64 @@ const questions = [
     type: 'input',
     message: 'What is the title of your project?',
     name: 'title',
-  },{
+  },
+  {
     type: 'input',
     message: 'Please write a short description of your project.',
-    name: 'decription',
-  },{
+    name: 'description',
+  },
+  {
     type: 'list',
     message: 'What type of license, if any, should your project have?',
     name: 'license',
-    choices: ["MIT", "GNU", "Apache", "Mozilla", "BSD", "none"]
-  },{
+    choices: ['MIT', 'GNU', 'Apache', 'Mozilla', 'BSD', 'none'],
+  },
+  {
     type: 'input',
     message: 'What is your Github username?',
     name: 'userName',
-  },{
+  },
+  {
     type: 'input',
     message: 'What is your Github email address?',
     name: 'emailAddress',
-  },{
+  },
+  {
     type: 'input',
     message: 'What command should be run to install dependencies',
     name: 'command',
-  },{
+  },
+  {
     type: 'input',
     message: 'What command should run for testing?',
     name: 'tests',
-  },{
+  },
+  {
     type: 'input',
     message: 'What does the user need to know to about using the repo?',
     name: 'usage',
-  },{
+  },
+  {
     type: 'input',
     message: 'What does the user need to know about contributing to the repo?',
     name: 'contribution',
   },
 ];
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-  const writeToFile = (data) => { 
-    fs.writeToFile('questions', data, (err) => {
-      err ? console.error(err) : console.log('File created!')
-    })
+const writeToFile = (fileName, data) => {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) { console.error(err);
+      }  else { console.log('File created!');
   }
-
+});
+};
 
 // TODO: Create a function to initialize app
-function init() {
-  inquirer.prompt(questions);
+const init = () => {
+  inquirer.prompt(questions)
+  .then(answers);
+  const readmeInfo = generateMarkdown(answers)
+  writeToFile('README.md', readmeInfo);
 }
 
 // Function call to initialize app
